c6de9b3ffe93c0ad921370d0dc3ed3cec5ea0947
Jose <161727897+joseaej@users.noreply.github.com>
2025-10-21 23:03:08 +0200

    created offline page

--- lib/presentation/auth/auth_checker.dart
+++ lib/presentation/auth/auth_checker.dart
@@ -1,38 +1,62 @@
+
 import 'package:calculix/presentation/auth/auth_notifier.dart';
 import 'package:calculix/presentation/auth/login_page.dart';
+import 'package:calculix/presentation/connection/no_connection_page.dart';
 import 'package:calculix/presentation/home/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:calculix/data/models/auth_state.dart';
+import 'package:internet_connection_checker_plus/internet_connection_checker_plus.dart';
 
-
-class AuthChecker extends ConsumerWidget {
+class AuthChecker extends ConsumerStatefulWidget {
   const AuthChecker({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    // 1. Escucha el estado de autenticación
-    final authState = ref.watch(authNotifierProvider);
+  ConsumerState<ConsumerStatefulWidget> createState() => _AuthCheckerState();
+}
 
-    // 2. Decide qué widget mostrar basado en el estado
-    switch (authState.status) {
-      case AuthStatus.authenticated:
-        // Si está autenticado, muestra la página de inicio
-        return const HomePage();
-        
-      case AuthStatus.unauthenticated:
-      case AuthStatus.error:
-        // Si no está autenticado o hay un error, muestra la página de login
-        return const LoginPage();
+class _AuthCheckerState extends ConsumerState<AuthChecker> {
+  bool _isConnectedToIntertnet = false;
+  @override
+  void initState() {
+    super.initState();
+    InternetConnection().onStatusChange.listen((event) {
+      switch (event) {
+        case InternetStatus.connected:
+          setState(() {
+            _isConnectedToIntertnet = true;
+          });
+          break;
+        case InternetStatus.disconnected:
+          setState(() {
+            _isConnectedToIntertnet = false;
+          });
+          break;
+      }
+    });
+  }
 
-      case AuthStatus.loading:
-      case AuthStatus.initial:
-      // Mientras carga o en el estado inicial, muestra un indicador de progreso
-        return const Scaffold(
-          body: Center(
-            child: CircularProgressIndicator(),
-          ),
-        );
+
+  @override
+  Widget build(BuildContext context) {
+    if (_isConnectedToIntertnet) {
+      final authState = ref.watch(authNotifierProvider);
+      switch (authState.status) {
+        case AuthStatus.authenticated:
+          return const HomePage();
+
+        case AuthStatus.unauthenticated:
+        case AuthStatus.error:
+          return const LoginPage();
+
+        case AuthStatus.loading:
+        case AuthStatus.initial:
+          return const Scaffold(
+            body: Center(child: CircularProgressIndicator()),
+          );
+      }
+    }else{
+      return NoConnectionPage();
     }
   }
 }
